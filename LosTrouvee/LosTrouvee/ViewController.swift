f11dba8a598264d3b150f8348a7b1603231da42f
Jari De Graeve <jari@MacBook-Jari.local>
2018-10-11 15:00:50 +0200

    Aanmaken project

---
+++ LosTrouvee/LosTrouvee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LosTrouvee
+//
+//  Created by Jari De Graeve on 11/10/2018.
+//  Copyright © 2018 Jari De Graeve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
